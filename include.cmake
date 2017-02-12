b4b35a1648ef614de0cc7536c3c64cefd0de177d
Justin <j.david.berger@gmail.com>
2017-02-11 17:35:29 -0700

    Copy the version specific to non version specific names

--- include.cmake
+++ include.cmake
@@ -14,3 +14,5 @@ else()
 endif()
 
 CGET_HAS_DEPENDENCY(Freetype NUGET_PACKAGE FreeType GIT git://git.sv.nongnu.org/freetype/freetype2.git VERSION VER-2-7-1 NO_FIND_VERSION OPTIONS -DBUILD_SHARED_LIBS:BOOL=true)
+CGET_EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy "${CGET_INSTALL_DIR}/lib/freetype271.lib" "${CGET_INSTALL_DIR}/lib/freetype.lib")
+CGET_EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy "${CGET_INSTALL_DIR}/lib/freetype271d.lib" "${CGET_INSTALL_DIR}/lib/freetyped.lib")
